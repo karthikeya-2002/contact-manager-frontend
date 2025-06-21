983704124bad4ae187962d71498bf00cb32d13ad
karthikeya-2002 <karthikbhat44123@gmail.com>
2025-06-21 12:45:54 +0530

    api commit

--- src/api.js
+++ src/api.js
@@ -3,5 +3,6 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://contact-manager-backend-g1r9.onrender.com/api/contacts'
 });
+// baseURL: 'https://contact-manager-backend-g1r9.onrender.com
 
 export default api;
